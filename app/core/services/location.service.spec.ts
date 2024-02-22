1278ae7cebc32975a3f6b509058f9d5ff300c079
Toon Bertier <toon.bertier@verhaert.digital>
2024-02-22 10:11:26 +0100

    wip: initial tests

--- app/core/services/location.service.spec.ts
+++ app/core/services/location.service.spec.ts
@@ -1,1 +1,89 @@
-/* TODO */
+import { Shallow } from 'shallow-render';
+import { CoreModule } from '../core.module';
+import { LocationService } from './location.service';
+import { take } from 'rxjs';
+
+describe('LocationService', () => {
+  let shallow: Shallow<LocationService>;
+
+  beforeEach(() => {
+    shallow = new Shallow(LocationService, CoreModule);
+  });
+
+  describe('userLocation$', () => {
+    describe('on success', () => {
+      it('should emit user location ', (done) => {
+        // @ts-ignore
+        navigator.geolocation = {
+          getCurrentPosition: (success: Function) => {
+            setTimeout(() => {
+              success({ coords: { latitude: 10, longitude: 10 } });
+            }, 500);
+          },
+        };
+
+        const { instance } = shallow.createService();
+
+        const expectation = [
+          {
+            available: false,
+            loading: true,
+          },
+          {
+            available: true,
+            loading: false,
+            location: [10, 10],
+          },
+        ];
+
+        const result: unknown[] = [];
+
+        instance.userLocation$.pipe(take(2)).subscribe((emission) => {
+          result.push(emission);
+
+          if (result.length === 2) {
+            expect(result).toEqual(expectation);
+            done();
+          }
+        });
+      });
+    });
+
+    describe('on error', () => {
+      it('should emit user location ', (done) => {
+        // @ts-ignore
+        navigator.geolocation = {
+          getCurrentPosition: (_: Function, error: Function) => {
+            setTimeout(() => {
+              error();
+            }, 500);
+          },
+        };
+
+        const { instance } = shallow.createService();
+
+        const expectation = [
+          {
+            available: false,
+            loading: true,
+          },
+          {
+            available: false,
+            loading: false,
+          },
+        ];
+
+        const result: unknown[] = [];
+
+        instance.userLocation$.pipe(take(2)).subscribe((emission) => {
+          result.push(emission);
+
+          if (result.length === 2) {
+            expect(result).toEqual(expectation);
+            done();
+          }
+        });
+      });
+    });
+  });
+});
