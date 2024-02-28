05821c69f0174c4920083ae5cfeaf1e3e9dde7fe
Dean Reymen <deenrayman@outlook.com>
2024-02-28 10:44:31 +0100

    TRAN-61  Unit tests in motivation part of the feedback form

--- app/modules/feedback/components/feedback-form/feedback-form.component.spec.ts
+++ app/modules/feedback/components/feedback-form/feedback-form.component.spec.ts
@@ -10,61 +10,45 @@ describe('FeedbackFormComponent', () => {
     shallow = new Shallow(FeedbackFormComponent, FeedbackModule);
   });
 
-  it('should go to the sub categories when selecting the main category', async () => {
+  it('feedback form flow', async () => {
     const { instance } = await shallow.render(`<app-feedback-form></app-feedback-form>`);
 
     expect(instance.currentFeedbackFormStep).toEqual(FeedbackFormStep.MAIN_CATEGORY);
+    expect(instance.activeStep).toEqual(0);
+    expect(instance.canClickNextButton()).toEqual(false);
 
     const mainCategory = instance.mainCategories[0].value;
     instance.feedbackForm.controls.mainCategory.setValue(mainCategory);
 
     expect(instance.currentFeedbackFormStep).toEqual(FeedbackFormStep.SUB_CATEGORY);
-  });
+    expect(instance.activeStep).toEqual(0);
+    expect(instance.canClickNextButton()).toEqual(false);
 
-  describe('canClickNextButton', () => {
-    let instance: FeedbackFormComponent;
+    const subCategory = instance.subCategories[0].value;
+    instance.feedbackForm.controls.subCategory.setValue(subCategory);
 
-    beforeEach(async () => {
-      instance = (await shallow.render(`<app-feedback-form></app-feedback-form>`)).instance;
-    });
+    expect(instance.currentFeedbackFormStep).toEqual(FeedbackFormStep.MOTIVATION);
+    expect(instance.activeStep).toEqual(1);
+    expect(instance.canClickNextButton()).toEqual(false);
 
-    it('should return true if the user filled in the information needed for the step', () => {
-      const mainCategory = instance.mainCategories[0].value;
-      instance.feedbackForm.controls.mainCategory.setValue(mainCategory);
+    const motivation = instance.motivations[0].value;
+    instance.feedbackForm.controls.motivation.setValue(motivation);
 
-      instance.back();
+    expect(instance.currentFeedbackFormStep).toEqual(FeedbackFormStep.MESSAGE_AND_ATTACHMENTS);
 
-      expect(instance.canClickNextButton()).toBe(true);
-    });
+    instance.back();
 
-    it('should return true if the user didnt fill in the information needed for the step', () => {
-      expect(instance.canClickNextButton()).toBe(false);
-    });
-  });
+    expect(instance.currentFeedbackFormStep).toEqual(FeedbackFormStep.MOTIVATION);
+    expect(instance.canClickNextButton()).toEqual(true);
 
-  describe('next', () => {
-    it('should go to the sub categories when selecting the main category', async () => {
-      const { instance } = await shallow.render(`<app-feedback-form></app-feedback-form>`);
+    instance.back();
 
-      expect(instance.currentFeedbackFormStep).toEqual(FeedbackFormStep.MAIN_CATEGORY);
+    expect(instance.currentFeedbackFormStep).toEqual(FeedbackFormStep.SUB_CATEGORY);
+    expect(instance.canClickNextButton()).toEqual(true);
 
-      const mainCategory = instance.mainCategories[0].value;
-      instance.feedbackForm.controls.mainCategory.setValue(mainCategory);
+    instance.back();
 
-      expect(instance.currentFeedbackFormStep).toEqual(FeedbackFormStep.SUB_CATEGORY);
-    });
-  });
-
-  describe('activeStep', () => {
-    it('should return 0 when the user is selecting the categories', async () => {
-      const { instance } = await shallow.render(`<app-feedback-form></app-feedback-form>`);
-
-      expect(instance.activeStep).toEqual(0);
-
-      const mainCategory = instance.mainCategories[0].value;
-      instance.feedbackForm.controls.mainCategory.setValue(mainCategory);
-
-      expect(instance.activeStep).toEqual(0);
-    });
+    expect(instance.currentFeedbackFormStep).toEqual(FeedbackFormStep.MAIN_CATEGORY);
+    expect(instance.canClickNextButton()).toEqual(true);
   });
 });
