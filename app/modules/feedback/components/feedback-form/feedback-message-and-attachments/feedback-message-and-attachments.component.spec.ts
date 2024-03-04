b05609f936e597150b5eb49b01d5ace40b79d44b
Dean Reymen <deenrayman@outlook.com>
2024-03-04 12:56:28 +0100

    TRAN-60 unit tests

---
+++ app/modules/feedback/components/feedback-form/feedback-message-and-attachments/feedback-message-and-attachments.component.spec.ts
@@ -0,0 +1,68 @@
+import { Shallow } from 'shallow-render';
+import { FeedbackModule } from '../../../feedback.module';
+import { FeedbackMessageAndAttachmentComponent } from './feedback-message-and-attachments.component';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { FeedbackAttachmentFileComponent } from './feedback-attachment-file/feedback-attachment-file.component';
+
+fdescribe('FeedbackMessageAndAttachmentComponent', () => {
+  let shallow: Shallow<FeedbackMessageAndAttachmentComponent>;
+
+  const reasonForm = new FormGroup({
+    message: new FormControl<string | null>(null, Validators.required),
+    publish: new FormControl<boolean | null>(null),
+    files: new FormArray<FormControl<File>>([]),
+  });
+
+  beforeEach(() => {
+    shallow = new Shallow(FeedbackMessageAndAttachmentComponent, FeedbackModule);
+  });
+
+  it('should not render any files if the files form value is empty', async () => {
+    const { find, fixture } = await shallow.render(
+      `
+        <app-feedback-message-and-attachments 
+          [reasonForm]="reasonForm"
+        ></app-feedback-message-and-attachments>
+      `,
+      {
+        bind: { reasonForm },
+      },
+    );
+
+    expect(find('app-feedback-attachment-file')).not.toHaveFoundOne();
+  });
+
+  it('should update files when a file is added and remove it when delete is pressed', async () => {
+    const fileName = 'image.jpg';
+
+    const { find, instance, fixture, findComponent } = await shallow.render(
+      `
+        <app-feedback-message-and-attachments 
+          [reasonForm]="reasonForm"
+        ></app-feedback-message-and-attachments>
+      `,
+      {
+        bind: { reasonForm },
+      },
+    );
+
+    expect(instance.reasonForm.value.files?.length).toBe(0);
+
+    instance.onFileInput({
+      target: { files: [new File([''], fileName, { type: 'image/jpeg' })] },
+    } as any);
+
+    fixture.detectChanges();
+
+    expect(find('app-feedback-attachment-file')).toHaveFoundOne();
+    expect(findComponent(FeedbackAttachmentFileComponent).name).toBe(fileName);
+    expect(instance.reasonForm.value.files?.length).toBeGreaterThan(0);
+
+    findComponent(FeedbackAttachmentFileComponent).onRemove.emit();
+
+    fixture.detectChanges();
+
+    expect(find('app-feedback-attachment-file')).not.toHaveFoundOne();
+    expect(instance.reasonForm.value.files?.length).toBe(0);
+  });
+});
