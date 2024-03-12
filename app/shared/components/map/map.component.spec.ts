2de5fb0b35ad677520727a39c5f78697ccdd4a74
Dean Reymen <deenrayman@outlook.com>
2024-03-12 09:20:32 +0100

    TRAN-14 unit tests

--- app/shared/components/map/map.component.spec.ts
+++ app/shared/components/map/map.component.spec.ts
@@ -6,7 +6,9 @@ import { of } from 'rxjs';
 import * as leaflet from 'leaflet';
 import { SimpleChange } from '@angular/core';
 import { environment } from '@environments/environment';
+import { MapService } from './map.service';
 import { LatLong } from '@core/models/location';
+import { DATA_POINT_QUALITY_COLOR_CHART, DataPointQuality } from '@core/models/data-point';
 
 describe('MapComponent', () => {
   let shallow: Shallow<MapComponent>;
@@ -14,38 +16,45 @@ describe('MapComponent', () => {
   beforeEach(() => {
     shallow = new Shallow(MapComponent)
       .mock(HttpClient, {
-        get: () => of('<svg></svg>'),
+        get: () => of('<svg fill="currentColor" stroke="strokeColor"></svg>'),
       })
       .replaceModule(HttpClientModule, HttpClientTestingModule);
   });
 
-  describe('center and zoom', () => {
-    it('should set default center and zoom when input is not defined', async () => {
-      const { instance, fixture } = await shallow.render();
+  describe('zoom', () => {
+    it('should set zoom when input is not defined', async () => {
+      const { instance } = await shallow.render();
       const defaultLocation = environment.defaultLocation;
 
       expect(instance.map?.getCenter()).toEqual({ lat: defaultLocation[0], lng: defaultLocation[1] });
-      expect(instance.map?.getZoom()).toEqual(13);
-
-      const newLocation = [1, 2] as LatLong;
-      instance.center = newLocation;
-      instance.ngOnChanges({
-        center: new SimpleChange(environment.defaultLocation, instance.center, false),
-      });
-
-      await fixture.whenStable();
-
-      expect(instance.map?.getCenter()).toEqual({ lat: newLocation[0], lng: newLocation[1] });
+      expect(instance.map?.getZoom()).toBe(13);
     });
 
-    it('should set center and zoom from input', async () => {
-      const location = [1, 1] as LatLong;
+    it('should set zoom from input', async () => {
       const zoom = 2;
 
-      const { instance } = await shallow.render({ bind: { center: location, zoom } });
+      const { instance } = await shallow.render({ bind: { zoom } });
 
-      expect(instance.map?.getCenter()).toEqual({ lat: location[0], lng: location[1] });
-      expect(instance.map?.getZoom()).toEqual(zoom);
+      expect(instance.map?.getZoom()).toBe(zoom);
+    });
+  });
+
+  describe('center', () => {
+    it('should set initial center as the default environment value', async () => {
+      const { instance } = await shallow.render();
+      const defaultLocation = environment.defaultLocation;
+
+      expect(instance.map?.getCenter()).toEqual({ lat: defaultLocation[0], lng: defaultLocation[1] });
+    });
+
+    it('should update the center when a new point is set in the map service', async () => {
+      const { instance, inject } = await shallow.render();
+      const mapService = inject(MapService);
+
+      const newCenter = [0, 2] as LatLong;
+      mapService.setCenter(newCenter);
+
+      expect(instance.map?.getCenter()).toEqual({ lat: newCenter[0], lng: newCenter[1] });
     });
   });
 
@@ -53,41 +62,50 @@ describe('MapComponent', () => {
     it('when marker only has the location property', async () => {
       const markers: Marker[] = [{ location: [0, 0] }, { location: [1, 1] }];
 
-      const { fixture, instance } = await shallow.render({ bind: { markers, center: [0, 0] } });
+      const { fixture, instance } = await shallow.render({ bind: { markers } });
       await fixture.whenStable();
 
-      expect(instance.markers.length).toEqual(markers.length);
+      expect(instance.markers.length).toBe(markers.length);
     });
 
     it('when the marker input is updated it should shown the correct updated markers', async () => {
       const markers: Marker[] = [
-        { location: [0, 0], active: false },
+        { location: [0, 0], active: false, color: DATA_POINT_QUALITY_COLOR_CHART[DataPointQuality.GOOD] },
         { location: [1, 1], active: false },
       ];
-
-      const { fixture, instance } = await shallow.render({ bind: { markers, center: [0, 0] } });
-
+      const { find, fixture, instance } = await shallow.render({ bind: { markers } });
       await fixture.whenStable();
 
-      expect(instance.markers.length).toEqual(markers.length);
+      const firstMarkerHTML = (find('.leaflet-marker-icon')[0].nativeElement as HTMLElement).innerHTML;
+      const secondMarkerHTML = (find('.leaflet-marker-icon')[1].nativeElement as HTMLElement).innerHTML;
 
-      const newMarkers: Marker[] = [{ location: [0, 0], active: true }];
+      expect(find('.leaflet-marker-icon').length).toBe(markers.length);
+      expect(getFillHexCode(firstMarkerHTML)).toBe(DATA_POINT_QUALITY_COLOR_CHART[DataPointQuality.GOOD]);
+      expect(getStrokeHexColor(firstMarkerHTML)).toBe(DATA_POINT_QUALITY_COLOR_CHART[DataPointQuality.GOOD]);
+      expect(getFillHexCode(secondMarkerHTML)).toBe(DATA_POINT_QUALITY_COLOR_CHART[DataPointQuality.DEFAULT]);
+      expect(getStrokeHexColor(secondMarkerHTML)).toBe(DATA_POINT_QUALITY_COLOR_CHART[DataPointQuality.DEFAULT]);
+
+      const newMarkers: Marker[] = [
+        { location: [0, 0], color: DATA_POINT_QUALITY_COLOR_CHART[DataPointQuality.POOR], active: true },
+      ];
       instance.markers = newMarkers;
       instance.ngOnChanges({
         markers: new SimpleChange(markers, newMarkers, false),
       });
-
       await fixture.whenStable();
 
-      expect(instance.markers.length).not.toEqual(markers.length);
-      expect(instance.markers.length).toEqual(newMarkers.length);
+      const markerHTML = (find('.leaflet-marker-icon').nativeElement as HTMLElement).innerHTML;
+
+      expect(find('.leaflet-marker-icon').length).toBe(newMarkers.length);
+      expect(getFillHexCode(markerHTML)).toBe(DATA_POINT_QUALITY_COLOR_CHART[DataPointQuality.POOR]);
+      expect(getStrokeHexColor(markerHTML)).toBe(DATA_POINT_QUALITY_COLOR_CHART[DataPointQuality.DEFAULT]);
     });
   });
 
   it('should emit markerClick event when a marker is clicked', async () => {
     const location = [0, 0] as [number, number];
     const markers: Marker[] = [{ location }];
-    const { instance, fixture } = await shallow.render({ bind: { markers, center: [0, 0] } });
+    const { instance, fixture } = await shallow.render({ bind: { markers } });
 
     await fixture.whenStable();
 
@@ -102,3 +120,13 @@ describe('MapComponent', () => {
     expect(instance.markerClick.emit).toHaveBeenCalledWith(location);
   });
 });
+
+function getFillHexCode(svgString: string): string | null {
+  const fillMatch = svgString.match(/fill="#([A-Fa-f0-9]{6})"/);
+  return fillMatch ? `#${fillMatch[1]}` : null;
+}
+
+function getStrokeHexColor(svgString: string): string | null {
+  const strokeMatch = svgString.match(/stroke="#([A-Fa-f0-9]{6})"/);
+  return strokeMatch ? `#${strokeMatch[1]}` : null;
+}
