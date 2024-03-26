c175a7d9d17ec2e9686d05df73dd5d57bea2e52c
Dean Reymen <deenrayman@outlook.com>
2024-03-26 18:03:20 +0300

    MAS-25 disable linting on __esModule and test stylelint

--- app/modules/feedback/components/feedback-form/feedback-message-and-attachments/feedback-message-and-attachments.component.spec.ts
+++ app/modules/feedback/components/feedback-form/feedback-message-and-attachments/feedback-message-and-attachments.component.spec.ts
@@ -8,6 +8,7 @@ import imageCompression from 'browser-image-compression';
 import { convertMegabytesToBytes } from '@shared/utils/file-utils';
 
 jest.mock('browser-image-compression', () => ({
+  // eslint-disable-next-line @typescript-eslint/naming-convention
   __esModule: true,
   default: jest.fn((file) => file),
 }));
