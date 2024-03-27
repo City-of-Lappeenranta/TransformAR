013d195884a95d188446a85bf25e37fc6f9d5756
Toon Bertier <toon.bertier@verhaert.digital>
2024-03-27 16:51:09 +0100

    feat: improve focus location

--- app/modules/dashboard/components/dashboard-map/dashboard-map.component.ts
+++ app/modules/dashboard/components/dashboard-map/dashboard-map.component.ts
@@ -4,13 +4,13 @@ import { FormControl } from '@angular/forms';
 import { DATA_POINT_QUALITY_COLOR_CHART, DATA_POINT_TYPE_ICON, DataPoint, WeatherDataPoint } from '@core/models/data-point';
 import { LatLong } from '@core/models/location';
 import { DataPointsApi } from '@core/services/datapoints-api.service';
-import { LocationService } from '@core/services/location.service';
+import { LocationService, UserLocation } from '@core/services/location.service';
 import { environment } from '@environments/environment';
 import { TranslateService } from '@ngx-translate/core';
 import { Marker } from '@shared/components/map/map.component';
 import { isSameLocation } from '@shared/utils/location-utils';
 import { MessageService } from 'primeng/api';
-import { BehaviorSubject, Observable, Subject, combineLatest, distinctUntilChanged, map, take } from 'rxjs';
+import { BehaviorSubject, Observable, Subject, combineLatest, map, take, withLatestFrom } from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-map',
@@ -26,14 +26,17 @@ export class DashboardMapComponent implements AfterViewInit {
   private _selectedDataPointSubject$: Subject<DataPoint | null> = new Subject<DataPoint | null>();
   public selectedDataPoint$: Observable<DataPoint | null> = this._selectedDataPointSubject$.asObservable();
 
-  public locationLoading$: Observable<boolean> | undefined;
+  public locationLoading$: Observable<boolean> = this.locationService.userLocation$.pipe(map(({ loading }) => loading));
   public locationPermissionState$: Observable<PermissionState> = this.locationService.locationPermissionState$;
 
   public locationFormControl = new FormControl<LatLong | null>(null);
 
   public readonly TOAST_KEY = 'loading';
+
   private _mapCenterSubject$ = new BehaviorSubject<LatLong>(environment.defaultLocation as LatLong);
   public mapCenter$ = this._mapCenterSubject$.asObservable();
+
+  private _focusLocation$ = new Subject<void>();
 
   private readonly destroyRef = inject(DestroyRef);
 
@@ -51,6 +54,10 @@ export class DashboardMapComponent implements AfterViewInit {
     combineLatest([this._weatherDataPointMarkersLoadingSubject$])
       .pipe(takeUntilDestroyed())
       .subscribe((loadingStates) => loadingStates.every((loading) => !loading) && this.closeLoadingDataToast());
+
+    this._focusLocation$
+      .pipe(withLatestFrom(this.locationService.userLocation$, this.locationPermissionState$), takeUntilDestroyed())
+      .subscribe(([_, userLocation, permissionState]) => this.onFocusLocation(userLocation, permissionState));
   }
 
   public ngAfterViewInit(): void {
@@ -73,6 +80,10 @@ export class DashboardMapComponent implements AfterViewInit {
     this._selectedDataPointSubject$.next(null);
   }
 
+  public onFocusLocationClick(): void {
+    this._focusLocation$.next();
+  }
+
   private async showLoadingDataToast(): Promise<void> {
     this.messageService.add({
       key: this.TOAST_KEY,
@@ -93,29 +104,14 @@ export class DashboardMapComponent implements AfterViewInit {
     }));
   }
 
-  public focusLocation(): void {
-    const userLocation$ = this.locationService.userLocation$;
-    this.locationLoading$ = userLocation$.pipe(map((userLocation) => userLocation.loading));
+  private onFocusLocation(userLocation: UserLocation, permissionState: PermissionState): void {
+    if (userLocation.location && permissionState === 'granted') {
+      this._mapCenterSubject$.next(userLocation.location);
+    }
 
-    combineLatest([userLocation$, this.locationPermissionState$])
-      .pipe(
-        map(([currentUserLocation, permissionState]) => ({ currentUserLocation, permissionState })),
-        distinctUntilChanged((prev, curr) => {
-          return (
-            prev.currentUserLocation.loading === curr.currentUserLocation.loading &&
-            prev.currentUserLocation.location === curr.currentUserLocation.location
-          );
-        }),
-      )
-      .subscribe(({ currentUserLocation, permissionState }) => {
-        if (currentUserLocation?.location && permissionState === 'granted') {
-          this._mapCenterSubject$.next(currentUserLocation.location as LatLong);
-        }
-
-        if (!currentUserLocation.loading && permissionState === 'denied') {
-          alert(this.translateService.instant('PERMISSIONS.LOCATION.DENIED.ALERT'));
-        }
-      });
+    if (!userLocation.loading && permissionState === 'denied') {
+      alert(this.translateService.instant('PERMISSIONS.LOCATION.DENIED.ALERT'));
+    }
   }
 
   private handleWeatherDataPoints(weatherDataPoints: WeatherDataPoint[]): void {
