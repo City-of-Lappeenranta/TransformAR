2042a8b7e4e96aeef4c8d7fcad368c8d452dd7b0
Toon Bertier <toon.bertier@verhaert.digital>
2024-04-10 13:55:16 +0200

    chore: clean up npm scripts

--- main.ts
+++ main.ts
@@ -15,10 +15,10 @@ if (environment.production) {
       }),
     ],
     // Performance Monitoring
-    tracesSampleRate: 0.1, //  Capture 100% of the transactions
+    tracesSampleRate: 0.1,
     // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    replaysSessionSampleRate: 0.1,
+    replaysOnErrorSampleRate: 1.0,
   });
 }
 
