0024bb2371c3b1a1add2102a8d038072165378ce
Toon Bertier <toon.bertier@verhaert.digital>
2024-04-25 13:57:51 +0200

    feat: use live API

--- app/core/services/datapoints-api/datapoints-api.service.ts
+++ app/core/services/datapoints-api/datapoints-api.service.ts
@@ -1,7 +1,5 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '@environments/environment';
-import { MockHttpClient } from '../mock-http-client';
-import { Observable, map } from 'rxjs';
 import {
   AIR_QUALITY_CONVERSION,
   DataPointQuality,
@@ -11,6 +9,9 @@ import {
   WeatherConditionDataPoint,
   WeatherStormWaterDataPoint,
 } from '@core/models/data-point';
+import { environment } from '@environments/environment';
+import { removeNil } from '@shared/utils/object-utils';
+import { Observable, map } from 'rxjs';
 import {
   DataPointEndpoint,
   ParkingResponse,
@@ -18,67 +19,84 @@ import {
   WeatherConditionsResponse,
   WeatherStormWaterResponse,
 } from './models';
-import { removeNil } from '@shared/utils/object-utils';
 
 @Injectable({ providedIn: 'root' })
 export class DataPointsApi {
   private baseUrl = `${environment.streetAiApiUrl}/${environment.streetAiApiJurisdictionId}`;
+  private apiKey = environment.streetAiApiKey;
+  private defaultHeaders = new HttpHeaders().append('X-Api-Key', this.apiKey);
 
-  public constructor(private readonly httpClient: MockHttpClient) {}
+  public constructor(private readonly httpClient: HttpClient) {}
 
   public getWeatherConditions(): Observable<WeatherConditionDataPoint[]> {
-    return this.httpClient.get<WeatherConditionsResponse>(`${this.baseUrl}/${DataPointEndpoint.WEATHER_CONDITIONS}`).pipe(
-      map((response) =>
-        response.map(({ name, latitude, longitude, dataRetrievedTimestamp, ...rest }) => ({
-          name: name,
-          location: [latitude, longitude],
-          lastUpdateOn: dataRetrievedTimestamp,
-          type: DataPointType.WEATHER_CONDITIONS,
-          quality: DataPointQuality.DEFAULT,
-          data: { ...removeNil(rest) },
-        })),
-      ),
-    );
+    return this.httpClient
+      .get<WeatherConditionsResponse>(`${this.baseUrl}/${DataPointEndpoint.WEATHER_CONDITIONS}`, {
+        headers: this.defaultHeaders,
+      })
+      .pipe(
+        map((response) =>
+          response.map(({ name, latitude, longitude, dataRetrievedTimestamp, ...rest }) => ({
+            name: name,
+            location: [latitude, longitude],
+            lastUpdateOn: dataRetrievedTimestamp,
+            type: DataPointType.WEATHER_CONDITIONS,
+            quality: DataPointQuality.DEFAULT,
+            data: { ...removeNil(rest) },
+          })),
+        ),
+      );
   }
 
   public getWeatherStormWater(): Observable<WeatherStormWaterDataPoint[]> {
-    return this.httpClient.get<WeatherStormWaterResponse>(`${this.baseUrl}/${DataPointEndpoint.WEATHER_STORM_WATER}`).pipe(
-      map((response) =>
-        response.map(({ name, latitude, longitude, ...rest }) => ({
-          name: name,
-          location: [latitude, longitude],
-          type: DataPointType.STORM_WATER,
-          quality: DataPointQuality.DEFAULT,
-          data: { ...removeNil(rest) },
-        })),
-      ),
-    );
+    return this.httpClient
+      .get<WeatherStormWaterResponse>(`${this.baseUrl}/${DataPointEndpoint.WEATHER_STORM_WATER}`, {
+        headers: this.defaultHeaders,
+      })
+      .pipe(
+        map((response) =>
+          response.map(({ name, latitude, longitude, ...rest }) => ({
+            name: name,
+            location: [latitude, longitude],
+            type: DataPointType.STORM_WATER,
+            quality: DataPointQuality.DEFAULT,
+            data: { ...removeNil(rest) },
+          })),
+        ),
+      );
   }
 
   public getWeatherAirQuality(): Observable<WeatherAirQualityDataPoint[]> {
-    return this.httpClient.get<WeatherAirQualityResponse>(`${this.baseUrl}/${DataPointEndpoint.WEATHER_AIR_QUALITY}`).pipe(
-      map((response) =>
-        response.map(({ name, latitude, longitude, measurementIndex }) => ({
-          name: name,
-          location: [latitude, longitude],
-          type: DataPointType.AIR_QUALITY,
-          quality: AIR_QUALITY_CONVERSION[measurementIndex] ?? DataPointQuality.DEFAULT,
-        })),
-      ),
-    );
+    return this.httpClient
+      .get<WeatherAirQualityResponse>(`${this.baseUrl}/${DataPointEndpoint.WEATHER_AIR_QUALITY}`, {
+        headers: this.defaultHeaders,
+      })
+      .pipe(
+        map((response) =>
+          response.map(({ name, latitude, longitude, measurementIndex }) => ({
+            name: name,
+            location: [latitude, longitude],
+            type: DataPointType.AIR_QUALITY,
+            quality: AIR_QUALITY_CONVERSION[measurementIndex] ?? DataPointQuality.DEFAULT,
+          })),
+        ),
+      );
   }
 
   public getParking(): Observable<ParkingDataPoint[]> {
-    return this.httpClient.get<ParkingResponse>(`${this.baseUrl}/${DataPointEndpoint.PARKING}`).pipe(
-      map((response) =>
-        response.map(({ name, latitude, longitude, availableSpots }) => ({
-          name: name,
-          location: [latitude, longitude],
-          type: DataPointType.PARKING,
-          quality: DataPointQuality.DEFAULT,
-          availableSpots,
-        })),
-      ),
-    );
+    return this.httpClient
+      .get<ParkingResponse>(`${this.baseUrl}/${DataPointEndpoint.PARKING}`, {
+        headers: this.defaultHeaders,
+      })
+      .pipe(
+        map((response) =>
+          response.map(({ name, latitude, longitude, availableSpots }) => ({
+            name: name,
+            location: [latitude, longitude],
+            type: DataPointType.PARKING,
+            quality: DataPointQuality.DEFAULT,
+            availableSpots,
+          })),
+        ),
+      );
   }
 }
