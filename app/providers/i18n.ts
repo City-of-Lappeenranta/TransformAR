de66e807adac4f0ac4da63fd114495c4460c5884
Toon Bertier <toon.bertier@verhaert.digital>
2024-04-30 14:18:07 +0200

    hotfix: catch http error

--- app/providers/i18n.ts
+++ app/providers/i18n.ts
@@ -1,14 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { EnvironmentProviders, importProvidersFrom } from '@angular/core';
 import { TranslateLoader, TranslateModule, TranslateModuleConfig } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { EMPTY, Observable, catchError } from 'rxjs';
+
+export class CustomLoader implements TranslateLoader {
+  public constructor(private http: HttpClient) {}
+  public getTranslation(lang: string): Observable<any> {
+    return this.http.get(`/assets/i18n/${lang}.json`).pipe(catchError(() => EMPTY));
+  }
+}
 
 const config: TranslateModuleConfig = {
   defaultLanguage: 'en',
   useDefaultLang: true,
   loader: {
     provide: TranslateLoader,
-    useFactory: (httpClient: HttpClient) => new TranslateHttpLoader(httpClient),
+    useFactory: (httpClient: HttpClient) => new CustomLoader(httpClient),
     deps: [HttpClient],
   },
 };
