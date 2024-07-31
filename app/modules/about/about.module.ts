61c7cc18749fb62f45ebe94c71681aaab1a5f93c
Toon Bertier <toon.bertier@verhaert.digital>
2024-07-31 12:15:11 +0200

    chore: added content

--- app/modules/about/about.module.ts
+++ app/modules/about/about.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { AboutPageComponent } from './components/about-page/about-page.component';
 import { AboutRoutingModule } from './about.routing';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
   declarations: [AboutPageComponent],
-  imports: [CommonModule, AboutRoutingModule],
+  imports: [CommonModule, AboutRoutingModule, TranslateModule.forChild()],
   providers: [MessageService],
 })
 export class AboutModule {}
