22684c842fda9a1aa6193522b51228d47a96c234
Smaïl Hammour <smail.hammour@verhaert.digital>
2025-05-16 10:38:43 +0200

    fix(tests): MapComponent

--- src/app/shared/components/map/map.component.ts
+++ src/app/shared/components/map/map.component.ts
@@ -186,12 +186,14 @@ export class MapComponent implements AfterViewInit, OnChanges, OnDestroy {
     const { location } = marker;
     const divIcon = await this.getMarkerDivIcon(marker);
 
-    leaflet
-      .marker(new leaflet.LatLng(...location), {
-        icon: divIcon,
-      })
-      .on('click', this.onClickMarker.bind(this))
-      .addTo(this.map as leaflet.Map);
+    if (this.map) {
+      leaflet
+        .marker(new leaflet.LatLng(...location), {
+          icon: divIcon,
+        })
+        .on('click', this.onClickMarker.bind(this))
+        .addTo(this.map);
+    }
   }
 
   private async getMarkerDivIcon(marker: Marker): Promise<leaflet.DivIcon> {
